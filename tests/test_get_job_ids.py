729d466f03ea258c1a2078f1590a88c7d3e9d592
Robpol86 <robpol86@gmail.com>
2015-10-17 18:16:39 -0700

    Additional tests for get_job_ids().

--- tests/test_get_job_ids.py
+++ tests/test_get_job_ids.py
@@ -4,7 +4,7 @@ from functools import partial
 
 import pytest
 
-from appveyor_artifacts import get_job_ids
+from appveyor_artifacts import get_job_ids, HandledError
 
 
 def mock_query_api(url, replies):
@@ -77,3 +77,65 @@ def test_multiple_jobs(monkeypatch):
     actual = get_job_ids(config)
     expected = (['spfxkimxcj6faq57', '932hipuhbdyoycpd', '5r05nfjcamvlss5o'], 'success')
     assert actual == expected
+
+
+def test_failed(monkeypatch):
+    """Test non-success status."""
+    replies = {
+        '/projects/user/repo/history?recordsNumber=10': dict(builds=[
+            {'branch': 'master', 'commitId': '9b1df471879c0caae0594539d0ad87aab06a1ecd', 'isTag': False, 'jobs': [],
+             'status': 'failed', 'version': '1.6.0.43'},
+        ]),
+    }
+    monkeypatch.setattr('appveyor_artifacts.query_api', partial(mock_query_api, replies=replies))
+
+    config = dict(
+        owner='user',
+        repo='repo',
+        tag='',
+        pull_request=None,
+        commit='9b1df471879c0caae0594539d0ad87aab06a1ecd',
+    )
+
+    actual = get_job_ids(config)
+    expected = ([], 'failed')
+    assert actual == expected
+
+
+def test_errors(monkeypatch, caplog):
+    """Test handled exceptions."""
+    replies = {
+        '/projects/user/repo/history?recordsNumber=10': dict(),
+        '/projects/user/repo/build/1.6.0.43': dict(),
+    }
+    monkeypatch.setattr('appveyor_artifacts.query_api', partial(mock_query_api, replies=replies))
+
+    config = dict(
+        owner='user',
+        repo='repo',
+        tag='',
+        pull_request=None,
+        commit='9b1df471879c0caae0594539d0ad87aab06a1ecd',
+    )
+
+    with pytest.raises(HandledError):
+        get_job_ids(config)
+    assert caplog.records()[-2].message == 'Bad JSON reply: "builds" key missing.'
+
+    replies['/projects/user/repo/history?recordsNumber=10']['builds'] = [
+        {'branch': 'master', 'commitId': '9b1df471879c0caae0594539d0ad87aab06a1ecd', 'isTag': False, 'jobs': [],
+         'status': 'success', 'version': '1.6.0.43'},
+    ]
+
+    with pytest.raises(HandledError):
+        get_job_ids(config)
+    assert caplog.records()[-2].message == 'Bad JSON reply: "build" key missing.'
+
+    replies['/projects/user/repo/build/1.6.0.43']['build'] = dict()
+
+    with pytest.raises(HandledError):
+        get_job_ids(config)
+    assert caplog.records()[-2].message == 'Bad JSON reply: "jobs" key missing.'
+
+    replies['/projects/user/repo/build/1.6.0.43']['build']['jobs'] = [{'jobId': 'spfxkimxcj6faq57'}]
+    assert get_job_ids(config) == (['spfxkimxcj6faq57'], 'success')
